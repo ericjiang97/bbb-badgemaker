07e2611960fbf23629c4f5189fd874536628168e
Eric <eric.jiang@monash.edu>
2019-06-03 15:00:13 +1000

    updated logic - fixed a few text calculation bugs

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-badge = Image.open("badge.png")
-draw = ImageDraw.Draw(badge)
-font = ImageFont.truetype("Avenir-Book.ttf", 24)
-draw.text((0, 0),"Sample Text",(3,20,36),font=font)
-badge.save('sample-out.png')
+def generateParticipant(first_name, surname):
+    name = f"{first_name} {surname}"
+    badge = Image.open("./resources/participant.png")
+    draw = ImageDraw.Draw(badge)
+    MyFont = ImageFont.truetype("Avenir-Book.ttf", 24)
+
+    w, h = draw.textsize(name, font=MyFont)
+    print(w, h)
+
+    draw.text(((400-w)*0.5 , 135),name,(3,20,36),font=MyFont)
+    badge.save(f'./output/participants/{first_name}-{surname}.png')
+
+if __name__ == "__main__":
+    generateParticipant('Eric', 'Jiang')
